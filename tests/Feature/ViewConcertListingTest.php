172eaafc12c7f63d9b7f4c96dc56ab718f8daae4
Pc Elementary Os 2021 <bubu@outlook.com.pe>
2021-04-02 08:26:12 -0500

    Testing Query scopes

--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -15,7 +15,7 @@ class ViewConcertListingTest extends TestCase
     use  DatabaseMigrations;
 
     /** @test */
-    function user_can_view_a_concert_listing()
+    function user_can_view_a_published_concert_listing()
     {
         // 1. Arrage
         // create a concert
@@ -31,6 +31,7 @@ class ViewConcertListingTest extends TestCase
             'state'                  => 'ON',
             'zip'                    => '17916',
             'additional_information' => 'For tickets, call (555) 555-5555.',
+            'published_at' => Carbon::parse('-1 week'),
         ]);
 
         // 2. Act
--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -12,7 +12,7 @@ use Tests\TestCase;
 class ConcertTest extends TestCase
 {
 
-//    use DatabaseMigrations, DatabaseTransactions;
+    use DatabaseMigrations, DatabaseTransactions;
 
     /** @test */
     function can_get_formatted_date()
@@ -44,5 +44,20 @@ class ConcertTest extends TestCase
         ]);
 
         $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
+    }
+
+
+    /** @test */
+    function concert_with_a_published_at_date_are_published()
+    {
+        $publishedConcertA = factory(Concert::class)->create([ 'published_at' => Carbon::parse('-1 week') ]);
+        $publishedConcertB = factory(Concert::class)->create([ 'published_at' => Carbon::parse('-1 week') ]);
+        $unpublishedConcert = factory(Concert::class)->create([ 'published_at' => null ]);
+
+        $publishedConcerts = Concert::published()->get();
+
+        $this->assertTrue($publishedConcerts->contains($publishedConcertA));
+        $this->assertTrue($publishedConcerts->contains($publishedConcertB));
+        $this->assertFalse($publishedConcerts->contains($unpublishedConcert));
     }
 }
